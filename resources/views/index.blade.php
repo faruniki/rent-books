eab5db77de7c96b5c50ca21606f3b1f3eb0678a1
faruniki <najibfahrunaakbar123@gmail.com>
2022-12-28 21:07:15 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,16 @@
 
 @section('content')
 
-<h1>apa kabro</h1>
+<link rel="stylesheet" href="{{asset('assets/css/index.css')}}">
+
+<div>
+    <center>
+            <div class="navbar">
+                <p class="judul-navbar">Lab Komputer</p>
+                <p class="addnew"><a href="#" class="linkadd">HOME</a> </p>
+                <p class="delete"><a href="#" class="linkdelete">MAJORS</a> </p>
+            </div>
+    </center>
+</div>
 
 @endsection
